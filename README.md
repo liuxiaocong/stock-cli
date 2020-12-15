8ddd1f5a180c182086498c0cfff8cc922e381ea1
Evan Liu <evan.liu@shopee.com>
2020-12-15 18:17:22 +0800

    Update readme

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 Base on sina api http://hq.sinajs.cn/list=sh601003,sz000001,sz300725,sh688029
 
+Git Rep: https://github.com/liuxiaocong/stock-cli
+
 ### Install
 - npm install cn-stock-cli -g
 - Then you will got '<b>stock</b>' commend installed globally
-- stock 601003,000001,300725,688029
+- Run 'stock 601003,000001,300725,688029' on your terminal
+
+### Update
+- 15/12/2020: Add up / down display
--- src/index.js
+++ src/index.js
@@ -2,31 +2,42 @@
 const minimist = require("minimist");
 const axios = require("axios");
 const iconv = require("iconv-lite");
-const showError = ()=>{
-  console.error('Input error, please follow format') 
-  console.log('\x1b[31m%s\x1b[0m','stock 601003,000001,300725,688029');
-}
-if(!process.argv || !process.argv.slice){
+const showError = () => {
+  console.error("Input error, please follow format");
+  console.log("\x1b[31m%s\x1b[0m", "stock 601003,000001,300725,688029");
+};
+if (!process.argv || !process.argv.slice) {
   showError();
   return;
 }
 const args = minimist(process.argv.slice(2));
 if (args && args["_"]) {
   const org = args["_"][0];
-  if(!org){
+  if (!org) {
     showError();
     return;
   }
-  const orgArr = org.split(",");
+  const orgArr = (org + "").split(",");
+  if (orgArr.length === 1) {
+    let k = orgArr[0] + "";
+    if (k.length < 6) {
+      let now = k.length;
+      while (now < 6) {
+        k = "0" + k;
+        now++;
+      }
+    }
+    orgArr[0] = k;
+  }
   const postArr = [];
   const varArr = [];
   orgArr.forEach((key) => {
     if (key.startsWith("6")) {
       postArr.push("sh" + key);
-      varArr.push("hq_str_sh" + key)
+      varArr.push("hq_str_sh" + key);
     } else {
       postArr.push("sz" + key);
-      varArr.push("hq_str_sz" + key)
+      varArr.push("hq_str_sz" + key);
     }
   });
   axios
@@ -40,13 +51,18 @@ if (args && args["_"]) {
     .then(function (response) {
       const map = {};
       let data = iconv.decode(response.data, "gb2312");
-      data = data.replace(/var /g,'map.');
+      data = data.replace(/var /g, "map.");
       eval(data);
-      varArr.map((item)=>{
-        const current = map[item].split(',');
-        console.log(current[0] + ':  ' + current[3]);
-      })
-      
+      varArr.map((item) => {
+        const current = map[item].split(",");
+        let rate = (((current[3] - current[2]) / current[1]) * 100).toFixed(
+          2
+        );
+        if(rate > 0){
+          rate = "+ " + rate;
+        }
+        console.log(current[0] + ":  " + current[3]  + "  " + rate);
+      });
     })
     .catch(function (error) {
       // handle error
